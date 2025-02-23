626472e98e1ba2cfa40a6ee22202549ea428e529
grathod14 <grathod14581@gmail.com>
2025-02-23 14:01:40 -0800

    Added implicit wait

--- src/test/java/SampleTest/BrowserTest.java
+++ src/test/java/SampleTest/BrowserTest.java
@@ -12,6 +12,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 
 public class BrowserTest {
@@ -22,16 +23,17 @@ public class BrowserTest {
 	@Before
 	public void beforeMethod() {
 		String configPath = System.getProperty("user.dir") + "\\src\\main\\resources\\config.properties";
-        try {
-            prop.load(new FileInputStream(configPath));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+		try {
+			prop.load(new FileInputStream(configPath));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
-	
+
 	@Test
 	public void getData()
 	{
@@ -40,6 +42,7 @@ public class BrowserTest {
 		driver.get("https://bing.com");
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 		try {
+			wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//div[contains(@class,'mic_cont icon')]"))));
 			wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//textarea[@inputmode='search']"))));
 			driver.findElement(By.xpath("//textarea[@inputmode='search']")).sendKeys(prop.get("SEARCH_KEY").toString()+Keys.RETURN);
 			List<WebElement> results=driver.findElements(By.xpath("//h2/a"));
